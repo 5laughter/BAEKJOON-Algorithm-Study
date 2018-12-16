b8f8409735fe615ea1660c30d72ec280af8a30d4
5laughter <5laughter.luna@gmail.com>
2018-12-17 03:41:13 +0900

    2739

---
+++ 2739/sovled.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int N, i;
+	cin >> N;
+	for (i = 1; i <= 9; i++) {
+		cout << N << " * " << i << " = " << N*i << endl;
+	}
+	return 0;
+}
